22bd5a1e99487bf8c5f23d85138d9bf6a8d94d73
andres <fer7458@gmail.com>
2021-09-14 15:53:37 -0500

    ok

--- lib/scenes/projects/projects_views.dart
+++ lib/scenes/projects/projects_views.dart
@@ -12,7 +12,27 @@ class _ProjectViewState extends State<ProjectView> {
   Widget build(BuildContext context) {
 
     return Container(
+      padding: EdgeInsets.all(15),
       color: Colors.white70,
+      child: SafeArea(
+         child: ListView(
+           children: [
+             Container(
+                child: Column(
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Image.asset("assets/logo_home.png",
+                            fit: BoxFit.cover),
+                      ],
+                    ),
+                  ],
+                ),
+           )
+           ],
+         ),
+      ),
     );
   }
 }
